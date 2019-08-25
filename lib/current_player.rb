a6e86f06d714f1a5c3c9dd4a42e2e1a550c85b9f
Satvik Kaul <satvik.1799@gmail.com>
2019-08-25 13:20:00 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      counter + = 1
+    end
+  end
+  count= 9 - counter
+  return count
+end 
+board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+turn_count(board1)
